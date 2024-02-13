ed31e49a7be6a134dd1e99a0f86155adbb3f4f2e
Igor Chuvatkin <igorchuvatkin@gmail.com>
2024-02-13 17:02:47 +0300

    Complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
+    static TaskManager taskManager;
+
     public static void main(String[] args) {
+
         System.out.println("Поехали!");
+
+        taskManager = new TaskManager();
+
     }
 }
